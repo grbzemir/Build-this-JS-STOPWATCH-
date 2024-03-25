3f42ca572e7c6ad81baa149e2243e34f633f0184
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2024-03-25 10:32:02 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,81 @@
+
+const display = document.getElementById("display");
+
+
+let timer = null; /*Zamanlayıcıyı tutacak olan değişken*/
+let startTime = 0; /*Başlangıç zamanı*/
+let elapsedTime = 0; /*Geçen zaman*/
+let isRunning = false; /*Kronometre çalışıyor mu?*/
+
+
+function start()
+
+{
+
+    if(!isRunning)
+
+    {
+
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10);
+        console.log(timer);
+        isRunning = true;
+
+    }
+
+}
+
+function pause()
+
+{
+
+    if(isRunning)
+
+    {
+
+        clearInterval(timer);
+        elapsedTime = Date.now() - startTime;
+        isRunning = false;
+
+    
+    }
+
+
+
+}
+
+
+
+function reset()
+
+{
+
+    clearInterval(timer);
+    startTime = 0;
+    elapsedTime = 0;
+    isRunning = false;
+    display.textContent = "00:00:00:00";
+
+
+
+}
+
+function update()
+{
+    
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
+
+    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+
+}
